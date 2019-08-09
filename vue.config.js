8ebc0216db982939dd2245f50ba32453c242e89c
yuchuangqi <2298984542@qq.com>
2019-08-09 16:29:28 +0800

    mock数据

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +18,29 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  // 模拟数据
+  devServer:{
+    before(app){
+      app.get('/api/seller',function(req,res){
+        res.json({
+          error:0,
+          data:seller
+        })
+      })
+      app.get('/api/goods',function(req,res){
+        res.json({
+          error:0,
+          data:goods
+        })
+      })
+      app.get('/api/ratings',function(req,res){
+        res.json({
+          error:0,
+          data:ratings
+        })
+      })
     }
   }
 }
